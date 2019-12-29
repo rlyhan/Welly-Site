3d0f396e92a81d10f2b1b43febf58197840e26fa
rlyhan <richard_han@live.com>
2019-12-29 20:35:43 +1300

    Fixing data fetch errors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const path = require('path')
 require('dotenv').config()
 
 const auth = require('./routes/api/auth')
