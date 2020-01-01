962f0e41db5a9798710f2a2ed64527d9639e3727
rlyhan <richard_han@live.com>
2020-01-01 22:53:20 +1300

    Changed name/directory of CSS

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,6 @@ import { Provider } from 'react-redux'
 import store from './store'
 import { loadUser } from './actions/authActions'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-
 class App extends Component {
 
   componentDidMount() {
