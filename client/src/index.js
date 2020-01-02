7b02fd4b0c7538d52e02f51d06fd7c8bbc893258
rlyhan <richard_han@live.com>
2020-01-02 21:01:46 +1300

    Changing font/Bootstrap import methods

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
