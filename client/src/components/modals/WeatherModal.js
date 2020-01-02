f6668c305405fdfc3c07577c72424f2676ab7564
rlyhan <richard_han@live.com>
2020-01-03 00:11:37 +1300

    Switching import order

--- client/src/components/modals/WeatherModal.js
+++ client/src/components/modals/WeatherModal.js
@@ -27,7 +27,7 @@ class WeatherModal extends Component {
       <>
         <div className="weather-panel bg-transparent border-0 text-center" onClick={this.toggle}>
           <img className="weather-icon" src={require(`../../images/weather-icons/${this.props.weather.weatherIcon}.png`)} />
-          <div className="weather-info text-white text-left">
+          <div className="weather-info text-white">
             <h2>{this.props.weather.airvisualInfo.current.weather.tp}°C</h2>
           </div>
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import WebFont from 'webfontloader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
